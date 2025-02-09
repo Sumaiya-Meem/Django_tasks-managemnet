9764074a9224593f75db9483948c5df9e981b335
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2025-02-09 13:48:29 +0600

    add dynamic url

--- tasks/views.py
+++ tasks/views.py
@@ -6,5 +6,9 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Welcome to task management project")
 
+def show_task(request):
+    return HttpResponse("This is our show task page")
 
-
+def show_specific_task(request,id):
+    print("id",id)
+    return HttpResponse(f"This is my specific {id} task")
