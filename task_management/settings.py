09dcecae402d0890b72e93534d17f20f4fbc617d
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2025-02-11 00:43:09 +0600

    connect postgresql successfully

--- task_management/settings.py
+++ task_management/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#  For SQLLite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# For Postgresql 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': 'meem23',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
